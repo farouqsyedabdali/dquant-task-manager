b9ff0a17eaaacee6b8f26621e235cdc6625486c4
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2025-09-27 21:28:38 -0400

    added the new vercel app to the desktop file

--- desktop/main/main.js
+++ desktop/main/main.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { autoUpdater } = require('electron-updater');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
-const APP_URL = process.env.APP_URL || 'https://your-app.vercel.app';
+const APP_URL = process.env.APP_URL || 'https://dquant-task-manager.vercel.app/';
 
 let mainWindow;
 
